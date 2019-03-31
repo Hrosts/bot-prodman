80ea6b46183805e68c3283f2d5f2edeefb705ba1
aleksandrtikh <tikhonovichalex@mail.ru>
2019-03-31 16:13:47 +0300

    Remove old commands

--- src/repository/commandRepository.js
+++ src/repository/commandRepository.js
@@ -1,12 +1,6 @@
-const alerts = require('./commands/alertsCommand');
-const invasions = require('./commands/invasionsCommand');
-const cetus = require('./commands/cetusCommand');
 const triforce = require('./commands/triforceCommand')
 
 const repository = {
-	'alerts': alerts,
-	'invasions': invasions,
-	'cetus': cetus,
 	'triforce': triforce
 };
 
